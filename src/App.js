dd043cab390587bf1ba9dec51d9b54af9dadeaf2
danieldz21 <daniel.da.silva.tj2011@gmail.com>
2022-07-20 13:58:20 -0300

    index.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
+import PasswordGenerator from "./components/PasswordGenerator";
+
 function App() {
   return (
-    <></>
+    <main>
+      <div class="container">
+        <h1 class="app-title">Gerador de Senhas</h1>
+
+        <PasswordGenerator />
+      </div>
+    </main>
   );
 }
 
